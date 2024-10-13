23ad4ab9779ccdbead2715ebabe7f4f205b31cac
Bishal <bishald288@gmail.com>
2024-10-13 15:08:38 +0530

    again 6

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def upload_file():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
 
+        print(file_path)
         proba = classify(file_path)
 
         if os.path.exists(file_path):
